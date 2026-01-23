b6e3de3e19b801f8b214f4afa4c52140148c2d7a
Nahla-KT <nahlaktnahla@gmail.com>
2026-01-22 22:16:32 -0800

    final

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function connection(){
-    const db = await mongoose.connect('mongodb+srv://nahlaktnahla_db_user:F7tS7vXPU0qmfKJZ@cluster0.5our2lg.mongodb.net//BACKENDEXPRESS')
+    const db = await mongoose.connect('mongodb+srv://nahlaktnahla_db_user:F7tS7vXPU0qmfKJZ@cluster0.5our2lg.mongodb.net/BACKENDEXPRESS')
     console.log("database created");
     
 
